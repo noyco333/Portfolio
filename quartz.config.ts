42e7cea139efd0e8eebc772e012d94412da537bf
noyco333 <85548334+noyco333@users.noreply.github.com>
2026-05-21 17:22:13 +0900

    style

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Ryan Moriya",
+    pageTitleSuffix: "Portfolio / Knowledgebase",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
